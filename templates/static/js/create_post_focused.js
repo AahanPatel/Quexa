1f32e521ee0645f7fb594ef6f3d6f35663584186
Ryan <ryanbjo187@gmail.com>
2025-02-01 17:52:38 -0500

    changes idk

---
+++ templates/static/js/create_post_focused.js
@@ -0,0 +1,12 @@
+function createPost() {
+
+    // TOPICS 
+
+    var numTopics;
+
+    for (var i = 0; i < numTopics; i++) {
+        const groupElementContainer = document.createElement('li');
+        groupElementContainer.classList.add('group-element-container');
+        groupElementContainer.dataset.color = iconColor;
+    }
+}
