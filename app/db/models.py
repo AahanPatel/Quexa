6c255e909fd403bcd50b14c45db88ba7213faa91
Aahan Patel <pate2206@purdue.edu>
2025-01-31 20:54:04 -0500

    Added database functionality and user model

---
+++ app/db/models.py
@@ -0,0 +1,17 @@
+from flask_login import UserMixin
+from datetime import datetime, date
+from main import db
+
+# User class
+class User(db.Model, UserMixin):
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    hours = db.Column(db.Float)
+    score = db.Column(db.Integer)
+
+    def __init__(self, name, password, hours, score):
+        self.name = name
+        self.password = password
+        self.hours = hours
+        self.score = score
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from app.db import create_db_instance
+
 
 app = Flask(__name__, template_folder="templates/html", static_folder="templates/static")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///main.db"
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-if __name__ == '__main__':
+with app.app_context():
+    db = create_db_instance()
+    db.init_app(app)
+    db.create_all()
     app.run(debug=True)
